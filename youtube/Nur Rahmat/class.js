d1715a5a405a7e81c22d1397f227d0a0751cae83
Andrey Sholinka <sholinka2111@gmail.com>
2023-02-13 06:55:30 +0700

    inheritance

--- youtube/Nur Rahmat/class.js
+++ youtube/Nur Rahmat/class.js
@@ -13,13 +13,25 @@ class Hewan {
     info(){
         this.kenalan()
         console.log(`Nama saya adalah ${this.nama}`);
-        console.log(`Umur adalah ${this.umur} tahun`);
+        console.log(`Umur ${this.umur} tahun`);
     }
 
 }
 
-const hewan1 = new Hewan("Chika", 1)
-console.log(hewan1.nama, hewan1.umur);
+// const hewan1 = new Hewan("Chika", 1)
+// console.log(hewan1.nama, hewan1.umur);
 
 // hewan1.kenalan();
-hewan1.info();
+// hewan1.info();
+
+// inheritance
+class Anjing extends Hewan{
+    suara(){
+        console.log("Pake duit lu dulu yak");
+    }
+
+}
+
+const anjing = new Anjing("Guguk", 5)
+anjing.info()
+anjing.suara()
